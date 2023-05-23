c6e061615f37af99cde0930b4959340a29d6a9d9
akhil4626 <134359603+akhil4626@users.noreply.github.com>
2023-05-23 12:50:09 +0530

    Update loopsAndConditionsalStatements.js

--- loopsAndConditionsalStatements.js
+++ loopsAndConditionsalStatements.js
@@ -4,39 +4,39 @@
 
 
 // if, if-else. else programme
-// var x = 11;
-// var y = 21;
-// var z = 30;
-// var a = 111;
-// var b = 121;
-// var c = 131;
+var x = 11;
+var y = 21;
+var z = 30;
+var a = 111;
+var b = 121;
+var c = 131;
 
-// if(x==10){
-//     console.log(x)
-// }
+ if(x==10){
+     console.log(x)
+ }
 
-// else  if (y ==20){
-//     console.log(y)
-// }
+ else  if (y ==20){
+     console.log(y)
+ }
 
-// else if (z == 31){
-//     console.log(z)
-// }
+ else if (z == 31){
+     console.log(z)
+ }
 
-// else  if (a == 11){
-//     console.log(a)
-// }
+ else  if (a == 11){
+     console.log(a)
+ }
 
-// else if (b == 12){
-//     console.log(b)
-// }
+ else if (b == 12){
+     console.log(b)
+ }
 
-// else if (c == 13){
-//     console.log(c)
-// }
-// else{
-//     console.log("nothing matched bro!!")
-// }
+ else if (c == 13){
+     console.log(c)
+ }
+ else{
+     console.log("nothing matched bro!!")
+ }
 
 // if programme with triple equal
 

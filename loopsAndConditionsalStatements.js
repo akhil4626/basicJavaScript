ecbc5961f9dd244bb00ad0e6ade6c5a185146f12
akhil4626 <134359603+akhil4626@users.noreply.github.com>
2023-05-23 12:32:42 +0530

    Update and rename operators.js to loopsAndConditionsalStatements.js
    this repo is all about for loop, && operaters and if, if-else, else conditionals

---
+++ loopsAndConditionsalStatements.js
@@ -0,0 +1,74 @@
+// var x = 40;
+// var y = 20;
+// var result = (x<y )||(x==40);
+// var results = (x<y )&&(x==40);
+// console.log(results)
+
+// console.log(result)
+
+// odd number programme
+// for(var i = 0; i<=10; i++){
+//   if (i%2==1){
+// console.log(i);
+
+//   }
+// }
+
+// even number programme
+// for(var i = 0; i<=10; i++){
+//   if (i%2==0){
+// console.log(i);
+
+//   }
+// }
+
+
+
+// if, if-else. else programme
+// var x = 11;
+// var y = 21;
+// var z = 30;
+// var a = 111;
+// var b = 121;
+// var c = 131;
+
+// if(x==10){
+//     console.log(x)
+// }
+
+// else  if (y ==20){
+//     console.log(y)
+// }
+
+// else if (z == 31){
+//     console.log(z)
+// }
+
+// else  if (a == 11){
+//     console.log(a)
+// }
+
+// else if (b == 12){
+//     console.log(b)
+// }
+
+// else if (c == 13){
+//     console.log(c)
+// }
+// else{
+//     console.log("nothing matched bro!!")
+// }
+
+// if programme with triple equal
+
+var rating = 5;
+if(rating === 3){
+    console.log("you are a superstar")
+}
+if(rating === 4){
+    console.log("you are a powerstar")
+}
+
+if(rating === 5){
+    console.log("you are a rebelstar")
+}

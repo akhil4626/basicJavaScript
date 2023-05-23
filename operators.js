2137e9b070e471ff3b0e7091e2b56468f8deb597
akhil4626 <134359603+akhil4626@users.noreply.github.com>
2023-05-23 11:06:32 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,45 @@
+// var x = 40;
+// var y = 20;
+// var result = (x<y )||(x==40);
+
+// console.log(result)
+
+
+// for(var i = 0; i<=10; i++){
+//   if (i%2==1){
+// console.log(i);
+
+//   }
+// }
+
+
+var x = 11;
+var y = 20;
+var z = 30;
+var a = 11;
+var b = 12;
+var c = 13;
+
+if(x==10){
+    console.log(x)
+}
+
+else if (y ==20){
+    console.log(y)
+}
+
+else if (z == 30){
+    console.log(z)
+}
+
+else if (a == 11){
+    console.log(a)
+}
+
+else if (b == 12){
+    console.log(b)
+}
+
+else if (c == 13){
+    console.log(c)
+}
